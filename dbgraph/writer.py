a2cdb620ebc83640be11470eff5d7cfae00d4f22
David Poisl <david@poisl.at>
2013-04-24 23:59:45 +0200

    added the first parts of the script, started a stub for .dot files and moved template definition for yEd

--- dbgraph/writer.py
+++ dbgraph/writer.py
@@ -10,14 +10,6 @@ from xml.etree import ElementTree
 import json
 
 
-COLUMN_TEMPLATE_NOINFO = "%(name)s - %(translated_type)s"
-COLUMN_TEMPLATE_NULLABLE_STRING = "NULL"
-COLUMN_TEMPLATE_NOTNULLABLE_STRING = "NOT NULL"
-COLUMN_TEMPLATE_INFO = COLUMN_TEMPLATE_NOINFO + " [%(info)s]"
-COLUMN_TEMPLATE_DEFAULT_STRING = "DEFAULT=%(default)s"
-COLUMN_TEMPLATE_NODEFAULT_STRING = None
-
-
 class Generic(object):
     def __init__(self, reader, filename):
         self.reader = reader
@@ -38,34 +30,48 @@ class Json(Generic):
 
 
 class YEd(Generic):
-    # TODO: TEMPLATES!
+    TABLE_lABEL_TEMPLATE = "%(name)s"
+    TABLE_DESCRIPTION_TEMPLATE = "%(description)s"
+    SCHEMA_DESCRIPTION_TEMPLATE = "%(description)s"
+    SCHEMA_LABEL_TEMPLATE = "%(name)s"
+    COLUMN_TEMPLATE = "%(name)s - %(type)s"
+    INFO_TEMPLATE = "[%s]"
+    INFO_LABELS = {"nullable": "NULL", "default": "DEFAULT=%(default)s"}
+    FKEY_LABEL_TEMPLATE = ("%(constraint_schema)s.%(constraint_name)s\n"
+            "From: %(src_schema)s.%(src_table)s (%(src_column)s)\n"
+            "To: %(dst_schema)s.%(dst_table)s %(dst_column)s)\n"
+            "Indexed: %(src_indexed)s\nNullable: %(src_nullable)s")
+
+    def _element(node, name, attributes):
+        node.append(node.makeelement(name, attributes))
+
     def _create_empty_document(self):
-        attributes = {"xmlns": "http://graphml.graphdrawing.org/xmlns",
-                      "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance", 
-                      "xmlns:y": "http://www.yworks.com/xml/graphml",
-                      "xmlns:yed": "http://www.yworks.com/xml/yed/3",
-                      "xsi:schemaLocation": "http://graphml.graphdrawing.org/xmlns "
-                         "http://www.yworks.com/xml/schema/graphml/1.1/ygraphml.xsd"
-                      }
-        root = ElementTree.Element("graphml", attributes)
-        root.append(root.makeelement("key", {"id": "nodegraphics", "for": "node", 
-                                             "yfiles.type": "nodegraphics"}))
-        root.append(root.makeelement("key", {"id": "edgegraphics", "for": "edge", 
-                                             "yfiles.type": "edgegraphics"}))
-        root.append(root.makeelement("key", {"id": "nodeurl", "for": "node", 
-                                             "attr.name": "url", 
-                                             "attr.type": "string"}))
-        root.append(root.makeelement("key", {"id": "nodedescription", "for": "node",
-                                             "attr.name": "description", 
-                                             "attr.type": "string"}))
-        root.append(root.makeelement("key", {"id": "edgeurl", "for": "edge", 
-                                             "attr.name": "url", 
-                                             "attr.type": "string"}))
-        root.append(root.makeelement("key", {"id": "edgedescription", "for": "edge",
-                                             "attr.name": "description", 
-                                             "attr.type": "string"}))
-        root.append(root.makeelement("graph", {"edgedefault": "directed", 
-                                             "id": "database"}))
+        attr = {"xmlns": "http://graphml.graphdrawing.org/xmlns",
+                "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance", 
+                "xmlns:y": "http://www.yworks.com/xml/graphml",
+                "xmlns:yed": "http://www.yworks.com/xml/yed/3",
+                "xsi:schemaLocation": "http://graphml.graphdrawing.org/xmlns "
+                   "http://www.yworks.com/xml/schema/graphml/1.1/ygraphml.xsd"
+                }
+        root = ElementTree.Element("graphml", attr)
+        self._element(root, "key", {"id": "nodegraphics", "for": "node", 
+                                    "yfiles.type": "nodegraphics"}))
+        self._element(root, "key", {"id": "edgegraphics", "for": "edge", 
+                                    "yfiles.type": "edgegraphics"}))
+        self._element(root, "key", {"id": "nodeurl", "for": "node", 
+                                    "attr.name": "url", 
+                                    "attr.type": "string"}))
+        self._element(root, "key", {"id": "nodedescription", "for": "node",
+                                    "attr.name": "description", 
+                                    "attr.type": "string"}))
+        self._element(root, "key", {"id": "edgeurl", "for": "edge", 
+                                    "attr.name": "url", 
+                                    "attr.type": "string"}))
+        self._element(root, "key", {"id": "edgedescription", "for": "edge",
+                                    "attr.name": "description", 
+                                    "attr.type": "string"}))
+        self._element(root, "graph", {"edgedefault": "directed", 
+                                      "id": "database"}))
         return root
 
     def _add_schema_label(self, node, label):
@@ -80,43 +86,39 @@ class YEd(Generic):
         group.append(label_node)
 
     def _generate_document(self, data):
-        TABLE_TITLE_TEMPLATE = "%(name)s"
-        TABLE_DESCRIPTION_TEMPLATE = "%(description)s"
-        SCHEMA_DESCRIPTION_TEMPLATE = "%(description)s"
-        SCHEMA_LABEL_TEMPLATE = "%(name)s"
-
         document = self._create_empty_document()
         graph = document.find("graph")
+
         for (schema_name, schema_data) in data["schemas"].items():
             schema_node = graph.makeelement("node", {"id": "schema-%s-node" % schema_name})
             graph.append(schema_node)
             
             schema_description = graph.makeelement("data", {"key": "nodedescription"})
-            schema_description.text = SCHEMA_DESCRIPTION_TEMPLATE % schema_data
+            schema_description.text = self.SCHEMA_DESCRIPTION_TEMPLATE % schema_data
             schema_node.append(schema_description)
             
-            self._add_schema_label(schema_node, SCHEMA_LABEL_TEMPLATE % schema_data)
+            self._add_schema_label(schema_node, self.SCHEMA_LABEL_TEMPLATE % schema_data)
             schema_graph = graph.makeelement("graph", {"id": "schema-%s-graph" % schema_name})
             schema_node.append(schema_graph)
 
             for (table_name, table_data) in schema_data["tables"].items():
                 # get text for table nodes
-                (attribute_text, lines) = self._table_description(table_data)
+                (attribute_text, line_count) = self._table_description(table_data)
                 # create table node!
-                node = graph.makeelement("node", {"id": "table-%s-%s" % (schema_name, table_name)})
+                node = graph.makeelement("node", {"id": "table-%s-%s" % (
+                                                  schema_name, table_name)})
                 schema_graph.append(node)
 
                 node_description = graph.makeelement("data", {"key": "nodedescription"})
-                node_description.text = TABLE_DESCRIPTION_TEMPLATE % table_data
+                node_description.text = self.TABLE_DESCRIPTION_TEMPLATE % table_data
                 node.append(node_description)
 
                 node_data = graph.makeelement("data", {"key": "nodegraphics"})
                 node.append(node_data)
                 gn = graph.makeelement("y:GenericNode", 
-                        {"configuration": "com.yworks.entityRelationship.big_entity",
-                            })
+                        {"configuration": "com.yworks.entityRelationship.big_entity"})
                 node_data.append(gn)
-                gn.append(graph.makeelement("y:Geometry", {"height": str(lines * 13 + 70),
+                gn.append(graph.makeelement("y:Geometry", {"height": str(line_count * 13 + 70),
                                                            "width": "500", 
                                                            "x": "0", "y": "0"}))
                 
@@ -125,7 +127,7 @@ class YEd(Generic):
                     "configuration": "com.yworks.entityRelationship.label.name",
                     "modelName": "internal",
                     "modelPosition": "t"})
-                label.text = table_name
+                label.text = self.TABLE_LABEL_TEMPLATE % table_data
                 gn.append(label)
                 
                 label = graph.makeelement("y:NodeLabel", {"alignment": "left", 
@@ -166,44 +168,39 @@ class YEd(Generic):
             tree.write(file_, "UTF-8", True)
 
     def _table_description(self, table, process_constraints=False):
-        COLUMN_TEMPLATE = "%(name)s - %(type)s"
-        INFO_TEMPLATE = "[%s]"
-        INFO_LABELS = {"nullable": "NULL", "default": "DEFAULT=%(default)s"}
-
         columns = table["columns"]
         constraints = table["constraints"]
         text = []
         for column in columns:
             info = []
             if column["nullable"]:
-                info.append(INFO_LABELS["nullable"])
+                info.append(self.INFO_LABELS["nullable"])
             if column["default"]:
                 info.append(INFO_LABELS["default"] % {"default": column["default"]})
-            col_text = COLUMN_TEMPLATE % column
+            col_text = self.COLUMN_TEMPLATE % column
             if len(info):
-                col_text += INFO_TEMPLATE % (", ".join(info))
+                col_text += self.INFO_TEMPLATE % (", ".join(info))
             text.append(col_text)
         return ("\n".join(text), len(text) + 1)
 
     def _generate_foreign_keys(self, graph, data):
-        FKEY_LABEL_TEMPLATE = ("%(constraint_schema)s.%(constraint_name)s\n"
-                "From: %(src_schema)s.%(src_table)s (%(src_column)s)\n"
-                "To: %(dst_schema)s.%(dst_table)s %(dst_column)s)\n"
-                "Indexed: %(src_indexed)s\nNullable: %(src_nullable)s")
         for fkey in data["foreign_keys"]:
             src_label = "table-%s-%s" % (fkey["src_schema"], fkey["src_table"])
             dst_label = "table-%s-%s" % (fkey["dst_schema"], fkey["dst_table"])
             label = fkey["constraint_schema"] + "." + fkey["constraint_name"]
-            description = FKEY_LABEL_TEMPLATE % fkey
+            description = self.FKEY_LABEL_TEMPLATE % fkey
             self._add_edge(graph, src_label, dst_label, label, description=description)
 
+
+class Dot(Generic):
+    pass #TODO: everything!
 
 if __name__ == "__main__":
     import reader
     r = reader.Json("test.json")
-    r = reader.PostgreSQL(host="db-test.intern.ewave.at", database="backoffice",
-                          user="ewave", password="ewave")
-    writer = Json(r, "test.json")
+#    r = reader.PostgreSQL(host="db-test.intern.ewave.at", database="backoffice",
+#                          user="ewave", password="ewave")
+#    writer = Json(r, "test.json")
     writer2 = YEd(r, "test.graphml")
     writer()
     writer2()
